e2d83a68c97aef735a2bb472d8cf21097c81bee1
shahriarshawon19 <shahriaribne49@gmail.com>
2025-11-10 11:31:15 +0600

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,56 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router';
+
+
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                 className="h-5 w-5" fill="none" 
+                 viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" 
+                    strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </div>
+
+          <ul tabIndex={-1} 
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li><Link to="/" className='gradient-btn'>Home</Link></li>
+            <li><Link to="/apps" className='gradient-btn'>Apps</Link></li>
+            <li><Link to="/installed" className='gradient-btn'>Installation</Link></li>
+          </ul>
         </div>
-    );
+
+        <Link to="/" className="w-10 text-xl flex items-center gap-2">
+          <img src="/logo.png" alt="logo" className="w-8 h-8" />
+          HERO.IO
+        </Link>
+      </div>
+
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-4">
+          <li><Link to="/" className='gradient-btn'>Home</Link></li>
+          <li><Link to="/apps" className='gradient-btn'>Apps</Link></li>
+          <li><Link to="/installed" className='gradient-btn'>Installation</Link></li>
+        </ul>
+      </div>
+
+      <div className="navbar-end">
+        <a   href="https://github.com/shahriarshawon19/my-first-github-repo"
+    target="_blank"
+    rel="noopener noreferrer" className="btn "  style={{
+      background: "linear-gradient(125.07deg, rgba(99, 46, 227, 1), rgba(159, 98, 242, 1) 100%)",
+      color: "white"
+    }}> <FontAwesomeIcon icon={faGithub} className="mr-2" /> Contribute</a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,42 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>i am home</h1>
+            <div className='mt-10'>
+                <h1 className='font-black text-5xl text-center'><span className='text-black'>We Build
+                </span> <br></br>
+                    <span className="text-5xl font-bold bg-gradient-to-r from-purple-700 to-purple-400 bg-clip-text text-transparent">Productive</span> <span>Apps</span></h1>
+                    <p className='text-center text-gray-400 mx-70 mt-4'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+                    <div className="flex justify-center items-center gap-4 mt-6">
+  <a
+    href="https://play.google.com/store"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img
+      src="../play.png"
+      alt="Google Play"
+      className="h-12"
+    />
+  </a>
+  <a
+    href="https://www.apple.com/app-store/"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img
+      src="/public/store.png"
+      alt="App Store"
+      className="h-12"
+    />
+  </a>
+</div>
+
+            </div>
+            
+            <div>
+                <img src="/public/hero.png" alt="" className="mx-auto mt-10" />
+            </div>
+
         </div>
     );
 };
